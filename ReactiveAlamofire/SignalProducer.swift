bd015f2587201735a33e6cfa806ac22d968585a6
Andrew Procter <andrewprocter@gmail.com>
2017-02-08 15:50:26 -0500

    ReactiveSwift and Alamofire syntax updates

--- ReactiveAlamofire/SignalProducer.swift
+++ ReactiveAlamofire/SignalProducer.swift
@@ -9,33 +9,33 @@
 import Foundation
 
 import Alamofire
-import ReactiveCocoa
+import ReactiveSwift
 import Result
 
 extension Alamofire.DataResponse: Error {}
 
-public extension SignalType where Value: ResponseProducerResultType, Error: ResponseProducerResultType {
+public extension SignalProtocol where Value: ResponseProducerResultType, Error: ResponseProducerResultType {
     /**
         Parse the next response of `self` with given Alamofire ResponseSerializer
          - Parameter responseSerializer: the response serializer to apply on response from self signal
          - Returns: A Signal which produces Alamofire.Response with the desired format
      */
     
-    func parseResponse<T: ResponseSerializerType>(_ responseSerializer: T) -> Signal<Alamofire.Response<T.SerializedObject, T.ErrorObject>, Alamofire.Response<T.SerializedObject, T.ErrorObject>> {
+    func parseResponse<T: DataResponseSerializerProtocol>(_ responseSerializer: T) -> Signal<Alamofire.DataResponse<T.SerializedObject>, Alamofire.DataResponse<T.SerializedObject>> {
         return Signal { observer in
             return self.observe { event in
                 var respValue: ResponseProducerResultType!
                 switch event {
-                case .Interrupted:
+                case .interrupted:
                     observer.sendInterrupted()
                     return
-                case .Completed:
+                case .completed:
                     observer.sendCompleted()
                     return
-                case .Next(let value):
+                case .value(let value):
                     respValue = value
                     // We try to parse the response event the response has error, as looks like it's by design of Alamofire
-                case .Failed(let error):
+                case .failed(let error):
                     respValue = error
                 }
                 let result = responseSerializer.serializeResponse(
@@ -44,23 +44,23 @@ public extension SignalType where Value: ResponseProducerResultType, Error: Resp
                     respValue.data,
                     respValue.error
                 )
-                let resp = Response<T.SerializedObject, T.ErrorObject>(
+                let resp = DataResponse<T.SerializedObject>(
                     request: respValue.request,
                     response: respValue.response,
                     data: respValue.data,
                     result: result
                 )
                 if (result.isSuccess) {
-                    observer.sendNext(resp)
+                    observer.send(value: resp)
                 } else {
-                    observer.sendFailed(resp)
+                    observer.send(error: resp)
                 }
             }
         }
     }
 }
 
-public extension SignalProducerType where Value: Alamofire.Request, Error == NoError {
+public extension SignalProducerProtocol where Value: Alamofire.DataRequest, Error == NoError {
     /**
         Make a SignalProducer for generating response SignalProducer from self request of SignalProducer and return
          - Returns: A SignalProducer for generating response SignalProducer from request
@@ -77,21 +77,21 @@ public extension SignalProducerType where Value: Alamofire.Request, Error == NoE
          - Returns: A SignalProducer for generating response from request
      */
     
-    func responseProducer(_ strategy: ReactiveCocoa.FlattenStrategy = .Concat) -> SignalProducer<ResponseProducerResult, ResponseProducerResult> {
+    func responseProducer(_ strategy: ReactiveSwift.FlattenStrategy = .concat) -> SignalProducer<ResponseProducerResult, ResponseProducerResult> {
         return self
             .metaResponseProducer()
             .flatten(strategy)
     }
 }
 
-public extension SignalProducerType where Value: ResponseProducerResultType, Error: ResponseProducerResultType {
+public extension SignalProducerProtocol where Value: ResponseProducerResultType, Error: ResponseProducerResultType {
     /**
         Parse the next response of `self` with given Alamofire ResponseSerializer
          - Parameter responseSerializer: the response serializer to apply on responses from `self` SignalProducer
          - Returns: A SignalProducer which produces Alamofire.Response with the desired format
      */
     
-    func parseResponse<T: ResponseSerializerType>(_ responseSerializer: T) -> SignalProducer<Alamofire.Response<T.SerializedObject, T.ErrorObject>, Alamofire.Response<T.SerializedObject, T.ErrorObject>> {
+    func parseResponse<T: DataResponseSerializerProtocol>(_ responseSerializer: T) -> SignalProducer<Alamofire.DataResponse<T.SerializedObject>, Alamofire.DataResponse<T.SerializedObject>> {
         return self.lift { signal in
             return signal.parseResponse(responseSerializer)
         }
